2d9a04b41f122fd49c55764be813f3cb48f9434c
David Queen <dsqjr@msn.com>
2019-06-25 12:04:34 -0400

    all test working

--- src/List.test.js
+++ src/List.test.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import List from './List';
 import store from './STORE';
 
-describe('<List />', () => {
-    it ('renders without crashing', () => {
-        const div = document.createElement('div');
-        const list ={
+const list ={
             id: '1',
             header: 'First list',
             cardIds: [ 'a', 'b', 'e', 'f', 'g', 'j', 'l', 'm' ],
           };
+
+describe('<List />', () => {
+    it ('renders without crashing', () => {
+        const div = document.createElement('div');
+        
     ReactDOM.render((<List key= {list.id} title={list.header} cards={list.cardIds.map(id => store.allCards[id])} />), div);
            
         // List
@@ -19,5 +22,9 @@ describe('<List />', () => {
         // cards={list.cardIds.map(id => store.allCards[id])}
 
         ReactDOM.unmountComponentAtNode(div);
+    });
+    it('renderers the UI as expected',()=>{
+      const tree = renderer.create((<List key= {list.id} title={list.header} cards={list.cardIds.map(id => store.allCards[id])} />)).toJSON();
+      expect(tree).toMatchSnapshot();
     });
 });
